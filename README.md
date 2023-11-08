edbf695fb3beb07feb74e83c5de298727bd4ec95
YIHAN WANG <yw7451@nyu.edu>
2023-11-07 20:07:12 -0500

    gesture

--- README.md
+++ README.md
@@ -1,60 +1,23 @@
-# p5.js Template
+## Summary ##
+The provided code is utilizing the p5.js library and ML5.js ("https://unpkg.com/ml5@latest/dist/ml5.min.js") for a web-based application that incorporates a live video feed, image manipulation, and interaction using hand gestures captured through a camera. It allows different modes of interaction with the displayed image and video feed by detecting the index finger and thumb positions from the captured video.
 
-This is a README file that can be used to describe and document your assignment.
+## Original Idea ##
+I want to combine camera recognition with gestures to do some interactions.
 
-Markdown Cheatsheet (from [https://www.markdownguide.org/cheat-sheet/](https://www.markdownguide.org/cheat-sheet/)):
+## Process ##
+1. Hand Gesture Detection:
+Utilizes the Handpose model from ML5.js to detect the position of the index finger and thumb in the captured video.
+2. Interaction Modes:
+     - "v": View image mode - Shows a blurred version of the loaded image and tracks the corresponding area based on the index finger's position in the video feed.
+     - "f": Freehand drawing mode - Allows the user to draw freely on the image using the index finger's movement.
+     - "c": Circle drawing mode - Draws circles on the image based on the index finger's movement, with the circle's size based on the distance between the index finger and thumb.
+     - "e": Exits the view image, freehand drawing, or circle drawing mode and returns to displaying the original image.
+It requires the p5.js and ml5.js libraries to be linked in the HTML file.
 
----
----
+## What did you end up implementing ##
+I track the index finger's movements, display different interaction modes on the image, and allow users to manipulate the displayed content using their index finger in the video feed.
 
-# Heading1
-## Heading2
-### Heading3
-#### Heading4
-##### Heading5
-###### Heading6
+## Challenge ##
+1. Utilizing hand gesture recognition (in this case, detecting the index finger and thumb) might be challenging due to variations in lighting, hand positioning, or model accuracy. Calibration and addressing potential inaccuracies are essential. 
+2. Creating various interaction modes (view image, freehand drawing, circle drawing) involves complex logic to switch between these modes based on key inputs. Coordinating different modes and their functionalities can be intricate.
 
-**bold text**
-
-*italicized text*
-
-~~strikethrough text~~
-
-Ordered List:
-1. First item
-2. Second item
-3. Third item
-
-Unordered List:
-- First item
-- Second item
-- Third item
-
-`short code block`
-
-```
-extended code block
-fun() {
-  return 0
-}
-```
-
-Link:  
-[linked text](https://www.example.com)
-
-
-Image with url:  
-![image description](https://dm-gy-6063-2023f-d.github.io/assets/homework/02/clark-espaco-modulado-00.jpg)
-
-
-Image on repo:  
-![image description](./file-name.jpg)
-
-
-To start a new line, add two spaces at the end of a line, like this:  
-this is a new line.
-
-
-To start a new paragraph, leave an empty line between two lines of text.
-
-This is a new paragraph.
--- sketch.js
+++ sketch.js
@@ -1,6 +1,140 @@
+let img;
+let handpose, video;
+let hands = [];
+let viewImage = false;
+let freehandDrawing = false;
+let circleDrawing = false;
+// graphics buffers
+let viewPG, freehandPG, circlePG;
+
+function preload() {
+  // Load and display an image in the window. 
+  img = loadImage('assets/1687631452.jpg');
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  /* 1. Initial Setup */
+  // Create a display window.
+  createCanvas(1200, 600);
+  viewPG = createGraphics(600, 600);
+  img.resize(600, 600);
+
+  viewPG.image(img, 0, 0);
+  viewPG.filter(BLUR, 5);
+  freehandPG = createGraphics(600, 600);
+  circlePG = createGraphics(600, 600);
+  // The instant view of the camera should be displayed next to the image 
+  // and has the same size as the image.
+  video = createCapture(VIDEO);
+  video.size(width / 2, height);
+  video.hide();
+
+  /* 2. Index finger tip detection */
+  handpose = ml5.handpose(video, modelReady);
+  // This sets up an event that fills the global variable "predictions"
+  // with an array every time new hand poses are detected
+  handpose.on("predict", function (results) {
+    hands = results;
+  });
+}
+
+function modelReady() {
+  console.log('model ready');
+}
+
+function gotResults(results) {
+  detections = results;
 }
 
 function draw() {
+  image(img, 0, 0);
+  // In the view image mode, a blurred version of the image should be displayed. 
+  if (viewImage) image(viewPG, 0, 0);
+  // Enter the freehand drawing mode.
+  if (freehandDrawing) image(freehandPG, 0, 0);
+  // Enter the circle drawing mode.
+  if (circleDrawing) image(circlePG, 0, 0);
+
+  image(video, width / 2, 0, width / 2, height);
+  // If there is a hand
+  if (hands.length > 0) {
+    drawIndexFingerTip();
+  }
+}
+
+/* A small circle should be drawn on the camera view to indicate 
+the position of the detected index finger tip. */
+function drawIndexFingerTip() {
+  let index = hands[0].annotations.indexFinger;
+  fill(29, 53, 87);
+  noStroke();
+  // When you move your index finger in front of the camera, 
+  // the circle should keep following your finger tip.
+  // The top of the index finger is index 3
+  ellipse(width / 2 + index[3][0], index[3][1], 30);
+  // When your index finger tip is moving in the camera view, 
+  // a rectangular area around the position indicator in the blurred image 
+  // should display the corresponding region from the original image without blurring.
+  if (viewImage) {
+    copy(img, index[3][0] - 50, index[3][1] - 50, 100, 100, index[3][0] - 50, index[3][1] - 50, 100, 100);
+  }
+  // In the freehand drawing mode, a freehand trace should be drawn on the image
+  // according to the movement of your index finger tip.
+  if (freehandDrawing) {
+    freehandPG.stroke(241, 250, 238);
+    freehandPG.strokeWeight(20.0);
+    freehandPG.point(index[3][0], index[3][1]);
+  }
+  // When your index finger tip is moving in the camera view, 
+  // a circle should be drawn at the position indicator on the image.
+  if (circleDrawing) {
+    circlePG.noStroke();
+    // The circle should be filled with the pixel color
+    // at the location of the position indicator with a suitable degree of opacity. 
+    let pixelColor = img.get(index[3][0], index[3][1]);
+    circlePG.fill(pixelColor, 80);
+    // The radius of the circle should be adjusted in proportion
+    // to the distance between the tips of your thumb and index finger. 
+    let thumb = hands[0].annotations.thumb;
+    let distTips = dist(thumb[3][0], thumb[3][1], index[3][0], index[3][1]);
+    let radius = 0.5 * distTips;
+    circlePG.circle(index[3][0], index[3][1], radius);
+  }
+  // A corresponding position indicator should also be drawn on the image
+  // to follow the movement of your index finger tip. 
+  fill(230, 57, 70);
+  stroke(0);
+  square(index[3][0] - 15, index[3][1] - 15, 30, 10);
+}
+
+function keyReleased() {
+  /* 3. Viewing the image */
+  // When the ‘v’ key is pressed and released, 
+  // the interface should enter the view image mode. 
+  if (key === 'v' || key === 'V') viewImage = true;
+
+  /* 4. Freehand drawing on the image */
+  // When the ‘f’ key is pressed and released, 
+  // the interface should enter the freehand drawing mode. 
+  if (key === 'f' || key === 'F') freehandDrawing = true;
+
+  /* 5. Drawing circles on the image */
+  // When the ‘c’ key is pressed and released, 
+  // the interface should enter the circle drawing mode.
+  if (key === 'c' || key === 'C') {
+    // In this mode, the original image should be displayed.
+    viewImage = false;
+    freehandDrawing = false;
+    circleDrawing = true;
+  }
+
+  /* 6. Exiting the view image/freehand drawing/circle drawing mode */
+  // When the ‘e’ key is pressed and released, 
+  // the interface should exit from the view image / freehand drawing / circle drawing mode, 
+  // and the original image should be redisplayed. 
+  if (key === 'e' || key === 'E') {
+    viewImage = false;
+    freehandDrawing = false;
+    circleDrawing = false;
+  }
 }
